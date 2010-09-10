73120956e44782e135b1ea7b5d86ba6dc1aacab0
hakobe932 <hakobe@gmail.com>
2010-09-11 00:23:17 +0900

    oauth 用のコントローラ

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,65 @@
+require 'oauth'
+require 'json'
+require 'pp'
+
+class AuthController < ApplicationController
+  def self.consumer
+    OAuth::Consumer.new(
+      "om1i4JMQdgdwnbmokDBkNg",
+      "5T71pRJGQAyvStMHqlFil14HXk6rHW1RnBqY0EGtc",
+      { :site => "http://twitter.com/" }
+    )
+  end
+
+  def index
+  end
+
+  def oauth
+    request_token = AuthController.consumer.get_request_token(
+      #:oauth_callback => "http://#{request.host_with_port}/oauth_callback"
+      :oauth_callback => "http://localhost:3000/auth/oauth_callback"
+    )
+    logger.info 'hogeee'
+    session[:request_token] = request_token.token
+    session[:request_token_secret] = request_token.secret
+    redirect_to request_token.authorize_url
+    return
+  end
+ 
+  def oauth_callback
+    consumer = AuthController.consumer
+    request_token = OAuth::RequestToken.new(
+      consumer,
+      session[:request_token],
+      session[:request_token_secret]
+    )
+ 
+    access_token = request_token.get_access_token(
+      {},
+      :oauth_token => params[:oauth_token],
+      :oauth_verifier => params[:oauth_verifier]
+    )
+
+    pp access_token
+ 
+    response = consumer.request(
+      :get,
+      '/account/verify_credentials.json',
+      access_token, { :scheme => :query_string }
+    )
+    case response
+    when Net::HTTPSuccess
+      @user_info = JSON.parse(response.body)
+      unless @user_info['screen_name']
+        flash[:notice] = "Authentication failed"
+        redirect_to :action => :index
+        return
+      end
+    else
+      RAILS_DEFAULT_LOGGER.error "Failed to get user info via OAuth"
+      flash[:notice] = "Authentication failed"
+      redirect_to :action => :index
+      return
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Sushime::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
+  match 'auth' => 'auth#index'
+  match 'auth/oauth' => 'auth#oauth'
+  match 'auth/oauth_callback' => 'auth#oauth_callback'
+
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
