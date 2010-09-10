2ad3de58d685664b4b55ee4c264063b1a4c353ef
hakobe932 <hakobe@gmail.com>
2010-09-11 00:29:55 +0900

    add columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100910152015) do
+ActiveRecord::Schema.define(:version => 20100910152436) do
 
   create_table "users", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "screen_name"
+    t.string   "token"
+    t.string   "token_secret"
   end
 
 end
