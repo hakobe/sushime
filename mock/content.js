6bf0473ab57fd6060628370436898acb3496ece1
hitode909 <hitode909@gmail.com>
2010-09-10 23:57:55 +0900

    ユーザーアイコンかえる

--- mock/content.js
+++ mock/content.js
@@ -1,3 +1,23 @@
 $(function() {
-    console.log('welcome');
+    // bind
+    $("body").bind("user-changed", function(event, username) {
+        $("#user-image img").attr({
+            src: "http://api.dan.co.jp/twicon/" + username + "/bigger"
+        });
+        $("#user-find form input[name='username']").val(username);
+    });
+
+    // trigger
+    $("#user-find form").bind("submit", function() {
+        try {
+            var username = $(this).find("input[name='username']").val();
+            $("body").trigger("user-changed", username);
+        } catch(e) {
+            console.log(e);
+        }
+        return false;
+    });
+
+    // init
+    $("body").trigger("user-changed", "tsuda");
 });
