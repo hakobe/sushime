a75bc694efb8e8361053412eb4add28690f4ecbc
hakobe932 <hakobe@gmail.com>
2010-09-10 23:10:49 +0900

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sushime::Application.config.secret_token = '467fb9165b799028323c7de2094901e5adc154b6c074772394bf05ec0b9f0d32bb6f41f02bbe8de596ee21bb2d2015b1f6563878c5108a246eda4a592f4ce0b1'
